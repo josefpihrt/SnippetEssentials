ff790fb8adc6d08244eb0db1e596e762053ea9d8
Josef Pihrt <josef.pihrt@outlook.com>
2016-04-30 14:13:27 +0200

    project "SaveSnippetsToSingleFile" added

---
+++ tools/SaveSnippetsToSingleFile/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Pihrtsoft.Snippets;
+
+namespace Pihrtsoft.Snippets
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Specify snippet directory:");
+
+            string dirPath = Console.ReadLine();
+
+            if (!Directory.Exists(dirPath))
+            {
+                Console.WriteLine($"Directory not found: {dirPath}");
+                Console.ReadKey();
+                return;
+            }
+
+            var snippets = new List<Snippet>(LoadSnippets(dirPath).OrderBy(f => f.FilePath));
+
+            var snippetFile = new SnippetFile(Path.Combine(dirPath, "snippets.xml"));
+
+            if (File.Exists(snippetFile.FullName))
+            {
+                Console.WriteLine($"File already exists: {snippetFile.FullName}");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach (Snippet snippet in snippets)
+            {
+                string s = Path.GetDirectoryName(snippet.FilePath)
+                    .Replace(dirPath, string.Empty)
+                    .TrimStart(Path.DirectorySeparatorChar)
+                    .Replace(Path.DirectorySeparatorChar, '.');
+
+                snippet.Keywords.Add("category:" + s);
+
+                snippetFile.Snippets.Add(snippet);
+
+                string relativePath = snippet.FilePath.Replace(dirPath, string.Empty);
+                Console.WriteLine(relativePath);
+            }
+
+            var saveSettings = new SaveSettings();
+
+            SnippetSerializer.Serialize(snippetFile, saveSettings);
+
+            Console.WriteLine("FINISHED");
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<Snippet> LoadSnippets(string dirPath)
+        {
+            foreach (string dirPath2 in Directory.EnumerateDirectories(dirPath, "*", SearchOption.TopDirectoryOnly))
+            {
+                if (Path.GetFileName(dirPath2).StartsWith("_"))
+                    continue;
+
+                foreach (SnippetFile snippetFile in SnippetSerializer.DeserializeFiles(dirPath2, SearchOption.AllDirectories))
+                {
+                    foreach (Snippet snippet in snippetFile.Snippets)
+                        yield return snippet;
+                }
+            }
+        }
+    }
+}
