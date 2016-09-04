7d943bdd8100455c2f9c1667f1c5a63d03db8a2b
Josef Pihrt <josef.pihrt@outlook.com>
2016-09-04 15:08:01 +0200

    Update tools

--- tools/SnippetChecker.cmd
+++ tools/SnippetChecker.cmd
@@ -1,1 +1,1 @@
-SnippetChecker.exe "..\SnippetEssentials\SnippetEssentials.CSharp" "D:\SkyDrive\programming\Snippets\CSharp" "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC#\Snippets"
+SnippetChecker.exe "..\SnippetEssentials\SnippetEssentials.CSharp" "..\SnippetEssentials\SnippetEssentials.CSharp.Dev" "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC#\Snippets"
