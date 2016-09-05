57a82e06eea865eac77a1e6ed2c55898cb5c5802
Josef Pihrt <josef.pihrt@outlook.com>
2016-09-05 02:20:34 +0200

    Update SnippetChecker.cmd

--- tools/SnippetChecker.cmd
+++ tools/SnippetChecker.cmd
@@ -1,1 +1,2 @@
+@echo off
 SnippetChecker.exe "..\SnippetEssentials\SnippetEssentials.CSharp" "..\SnippetEssentials\SnippetEssentials.CSharp.Dev" "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC#\Snippets"
